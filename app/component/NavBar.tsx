02bf59711db79077efe20e5fe23e4d4400c0ceb7
Ihedoro Fortunatus O <ihedfortune@gmail.com>
2023-09-22 10:56:32 +0100

    dev branch

--- app/component/NavBar.tsx
+++ app/component/NavBar.tsx
@@ -14,10 +14,10 @@ export const NavBar = (props: Props) => {
           <div className='flex items-center md:space-x-10 lg:space-x-20'>
               <div className='font-semibold text-2xl'> <a href="/">Evergreen</a></div>
               <nav className='max-md:"hidden'>
-                  <ul className='flex items-center lg:space-x-10 space-x-7 opacity-90 text-[15px]'>
-                    <li><a href="/" className='w-full inline-block py-3'>Shop</a></li>
-                    <li><a href="filter" className='w-full inline-block py-3'>Filter</a></li>
-                    <li><a href="myproducts" className='w-full inline-block py-3'>My Products</a></li>
+                  <ul className='flex items-center lg:space-x-10 space-x-7 opacity-90 text-[17px] '>
+                    <li><a href="/" className='w-full inline-block py-3 hover:text-green-600'>Shop</a></li>
+                    <li><a href="filter" className='w-full inline-block py-3 hover:text-green-600'>Filter</a></li>
+                    <li><a href="myproducts" className='w-full inline-block py-3 hover:text-green-600'>My Products</a></li>
                     
                   </ul>
               </nav>
